018836c877bd410482e016a78fe6df92e39b386f
Nick Newhouse <nick.newhouse@gmail.com>
2018-07-27 15:44:42 -0400

    Change name over to claustrophobe

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './lib/slideR.js',
+  entry: './lib/claustrophobe.js',
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: './bundle.js',
